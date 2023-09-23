af6ebf08d43ace3a0a515c2c2d302e7ab2359988
corn <dalcon10280@gmail.com>
2023-09-24 04:52:59 +0900

    init

---
+++ build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    java
+    id("org.springframework.boot") version "3.1.4"
+    id("io.spring.dependency-management") version "1.1.3"
+}
+
+group = "site.ymango"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_21
+}
+
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
