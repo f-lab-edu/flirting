c39d0355e5de5c3f1ae0bdb67c09450c3ad0da69
corn <dalcon10280@gmail.com>
2023-09-25 22:48:09 +0900

    feat: add domain-rds

---
+++ domain/domain-rds/src/main/java/site/ymango/hello/HelloRepository.java
@@ -0,0 +1,8 @@
+package site.ymango.hello;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface HelloRepository extends JpaRepository<Hello, Long> {
+}
---
+++ domain/domain-rds/src/test/java/site/ymango/domainrds/DomainRdsApplicationTests.java
@@ -0,0 +1,13 @@
+package site.ymango.domainrds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DomainRdsApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
