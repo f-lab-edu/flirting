4c00fff631aaba4790ec481b90afbab011cd4c6d
corn <dalcon10280@gmail.com>
2023-10-19 22:28:29 +0900

    chore: redis 설치

--- domain/domain-rds/build.gradle.kts
+++ domain/domain-rds/build.gradle.kts
@@ -23,10 +23,12 @@ repositories {
 }
 
 dependencies {
+    implementation (project(":core"))
+
     api("org.springframework.boot:spring-boot-starter-data-jpa")
     runtimeOnly("com.mysql:mysql-connector-j")
 
-    api ("com.querydsl:querydsl-jpa:5.0.0")
+    api ("com.querydsl:querydsl-jpa:5.0.0:jakarta")
     implementation ("com.querydsl:querydsl-core:5.0.0")
     annotationProcessor("com.querydsl:querydsl-apt:5.0.0:jakarta")
     annotationProcessor("jakarta.persistence:jakarta.persistence-api")
