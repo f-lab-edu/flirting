8ea5116f1896b8fbf40a394e00373981c168ee93
corn <dalcon10280@gmail.com>
2023-10-22 02:27:53 +0900

    fix: 사용하지 않는 쿼리 제거 (포인트 만료 정산 쿼리)

--- domain/domain-rds/src/main/java/site/ymango/point/repository/PointHistoryCustomRepository.java
+++ domain/domain-rds/src/main/java/site/ymango/point/repository/PointHistoryCustomRepository.java
@@ -1,8 +1,5 @@
 package site.ymango.point.repository;
 
-import java.util.List;
-import site.ymango.point.entity.PointHistoryEntity;
 
 public interface PointHistoryCustomRepository {
-  List<PointHistoryEntity> findNotExpiredBonusPoint(Long pointWalletId);
 }
