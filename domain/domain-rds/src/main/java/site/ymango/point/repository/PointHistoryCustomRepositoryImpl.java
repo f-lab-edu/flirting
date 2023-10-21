e48dabbf65de612ae8d6f79fd1a7a2f38e356f1f
corn <dalcon10280@gmail.com>
2023-10-22 02:27:53 +0900

    fix: 사용하지 않는 쿼리 제거 (포인트 만료 정산 쿼리)

--- domain/domain-rds/src/main/java/site/ymango/point/repository/PointHistoryCustomRepositoryImpl.java
+++ domain/domain-rds/src/main/java/site/ymango/point/repository/PointHistoryCustomRepositoryImpl.java
@@ -13,16 +13,6 @@ import site.ymango.point.enums.TransactionType;
 @Repository
 @RequiredArgsConstructor
 public class PointHistoryCustomRepositoryImpl implements PointHistoryCustomRepository {
-  private final JPAQueryFactory queryFactory;
 
-  public List<PointHistoryEntity> findNotExpiredBonusPoint(Long pointWalletId) {
-    return queryFactory.select(pointHistoryEntity)
-        .from(pointHistoryEntity)
-        .leftJoin(pointHistoryEntity)
-        .on(pointHistoryEntity.pointHistoryId.eq(pointHistoryEntity.referenceId))
-        .where(pointHistoryEntity.pointWalletId.eq(pointWalletId))
-        .where(pointHistoryEntity.transactionType.eq(TransactionType.ADD_BONUS_POINT))
-        .where(pointHistoryEntity.referenceId.isNull())
-        .fetch();
-  }
+  private final JPAQueryFactory queryFactory;
 }
