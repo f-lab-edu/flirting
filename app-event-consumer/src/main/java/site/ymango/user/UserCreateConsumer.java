5fd139fac7d1f31092347fa9f70f9eb611ceccdd
corn <dalcon10280@gmail.com>
2023-11-02 23:08:08 +0900

    fix: kafka consumer groupId 별도로 지정

--- app-event-consumer/src/main/java/site/ymango/user/UserCreateConsumer.java
+++ app-event-consumer/src/main/java/site/ymango/user/UserCreateConsumer.java
@@ -10,7 +10,7 @@ import site.ymango.point.PointService;
 public class UserCreateConsumer {
   private final PointService pointService;
 
-  @KafkaListener(topics = "user_create", groupId = "group_1")
+  @KafkaListener(topics = "user_create", groupId = "user_create_group")
   public void userCreateListener(Long userId) {
     pointService.createPointWallet(userId);
   }
